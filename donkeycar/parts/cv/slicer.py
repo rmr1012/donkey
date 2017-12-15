cb4668a549582a895fab2159aef5574700e7cf55
Dennis Ren <rmr1012@users.noreply.github.com>
2017-12-14 21:28:07 -0800

    Update slicer.py

--- donkeycar/parts/cv/slicer.py
+++ donkeycar/parts/cv/slicer.py
@@ -3,18 +3,19 @@ import numpy as np
 
 class slicer():
     
-    def __init__(self):
+    def __init__(self,partition):
+        self.partition=partition
         print("slicer inited")
     def run(self, img_arr):
         #print("slicing..")
 
         start_row, start_col = int(0), int(0)
         # Let's get the ending pixel coordinates (bottom right of cropped top)
-        end_row, end_col = int(height * .5), int(width)
+        end_row, end_col = int(height * self.partition), int(width)
         img_arr_t= img_arr[start_row:end_row , start_col:end_col]
 
         # Let's get the starting pixel coordiantes (top left of cropped bottom)
-        start_row, start_col = int(height * .5), int(0)
+        start_row, start_col = int(height * self.partition), int(0)
         # Let's get the ending pixel coordinates (bottom right of cropped bottom)
         end_row, end_col = int(height), int(width)
         img_arr_b = img_arr[start_row:end_row , start_col:end_col]
