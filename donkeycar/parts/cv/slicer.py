f1bb130e98f121b2e6db9c27e2945866b7633dc0
Dennis Ren <rmr1012@users.noreply.github.com>
2017-12-14 21:17:33 -0800

    Update slicer.py

--- donkeycar/parts/cv/slicer.py
+++ donkeycar/parts/cv/slicer.py
@@ -4,10 +4,21 @@ import numpy as np
 class slicer():
     
     def __init__(self):
-        print("I'm tiny rick!!")
+        print("slicer inited")
     def run(self, img_arr):
-        print("bro lol")
-        img_arr_t,img_arr_b,crap = cv2.split(img_arr)
+        #print("slicing..")
+
+        start_row, start_col = int(0), int(0)
+        # Let's get the ending pixel coordinates (bottom right of cropped top)
+        end_row, end_col = int(height * .5), int(width)
+        img_arr_t= img_arr[start_row:end_row , start_col:end_col]
+
+        # Let's get the starting pixel coordiantes (top left of cropped bottom)
+        start_row, start_col = int(height * .5), int(0)
+        # Let's get the ending pixel coordinates (bottom right of cropped bottom)
+        end_row, end_col = int(height), int(width)
+        img_arr_b = img_arr[start_row:end_row , start_col:end_col]
+
         return img_arr_t,img_arr_b
 
 
